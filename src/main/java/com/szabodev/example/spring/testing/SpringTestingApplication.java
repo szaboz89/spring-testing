75ad46d5f588a73be34033cfdd0430618720b6f8
Zoltán Szabó <zoltan.szabo@atolldev.hu>
2019-10-21 10:52:11 +0200

    Initial commit for Spring Boot Application

---
+++ src/main/java/com/szabodev/example/spring/testing/SpringTestingApplication.java
@@ -0,0 +1,12 @@
+package com.szabodev.example.spring.testing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringTestingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringTestingApplication.class, args);
+    }
+}
